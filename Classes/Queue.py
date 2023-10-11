2f1b13aef52bb26329175a239b92a430ae1d989c
Cristian Reyes <ccreyesmolina@gmail.com>
2023-10-10 22:45:43 -0500

    Initial commit

---
+++ Classes/Queue.py
@@ -0,0 +1,15 @@
+class Queue:
+	def __init__(self) -> None:
+		self.queue = []
+	
+	def queue(value: int, self) -> None:
+		self.queue.append(value)
+	
+	def dequeue(quantity: int, self) -> None:
+		self.queue = self.queue[:quantity]
+	
+	def is_empty(self) -> bool:
+		return True
+	
+	def print(self) -> str:
+		return []
---
+++ Classes/Stack.py
@@ -0,0 +1,18 @@
+class Stack:
+	def __init__(self) -> None:
+		pass
+
+	def create():
+		return []
+	
+	def stack():
+		return []
+	
+	def unstack():
+		return []
+	
+	def is_empty():
+		return True
+	
+	def print():
+		return []
