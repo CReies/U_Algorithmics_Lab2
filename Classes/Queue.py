b5d4b5f45af85f03ec30dfde6574cf86c75b01df
Cristian Reyes <ccreyesmolina@gmail.com>
2023-10-10 22:54:26 -0500

    add modify method

--- Classes/Queue.py
+++ Classes/Queue.py
@@ -5,8 +5,15 @@ class Queue:
 	def queue(self, value: int) -> None:
 		self.queue.append(value)
 	
-	def dequeue(quantity: int, self) -> None:
-		self.queue = self.queue[:quantity]
+	def dequeue(self, quantity: int) -> None:
+		self.queue = self.queue[quantity:]
+
+	def modify(self, value: int,) -> None:
+		for i in range(len(self.queue)):
+			if self.queue[i] == value: break
+			else: 
+				self.dequeue(1)
+		
 	
 	def is_empty(self) -> bool:
 		return self.queue == []
