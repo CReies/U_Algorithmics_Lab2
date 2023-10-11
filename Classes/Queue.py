31d3980ec1169625672428ee7dbee2849b48db12
Cristian Reyes <ccreyesmolina@gmail.com>
2023-10-11 00:05:30 -0500

    fix dequeue and unstack methods

--- Classes/Queue.py
+++ Classes/Queue.py
@@ -1,1 +1,26 @@
+class Queue:
+	def __init__(self) -> None:
+		self.data = []
+
+	def getData(self):
+		return self.data
+	
+	def queue(self, value: int) -> None:
+		self.data.append(value)
+	
+	def dequeue(self, quantity: int) -> None:
+		self.data = self.data[quantity:]
+
+	def modify(self, value: int) -> None:
+		if self.data[0] == value: return
+		else: 
+			self.dequeue(1)
 			self.modify(value)
+		
+	
+	def is_empty(self) -> bool:
+		return self.data == []
+	
+	def print(self) -> None:
+		print(self.data)
+
